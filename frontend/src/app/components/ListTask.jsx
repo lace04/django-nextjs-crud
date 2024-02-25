fdfd16a05e2fbb9373877d434d811a06c5023714
lace04 <luisace04@gmail.com>
2024-02-25 09:21:25 -0500

    Refactor ListTask component to fetch and display tasks

--- frontend/src/app/components/ListTask.jsx
+++ frontend/src/app/components/ListTask.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { TaskItem } from './TaskItem';
 
-function ListTask() {
-  return <div>ListTask</div>;
+async function loadTasks() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks/`);
+  const tasks = await res.json();
+  return tasks;
+}
+
+async function ListTask() {
+  const tasks = await loadTasks();
+
+  return (
+    <section className='w-full bg-zinc-800 p-4'>
+      <h2 className='text-2xl font-semibold text-center'>List Tasks</h2>
+      <div className='mb-10'>
+        {tasks.length === 0 ? (
+          <p className='text-center text-white text-xl'>No tasks to show</p>
+        ) : (
+          tasks.map((task) => <TaskItem key={task.id} task={task} />)
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default ListTask;
